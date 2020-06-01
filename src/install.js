8695b9435b0d254a1a10b64570f76c65591398b7
YING WANG <42584020+yingwang-us@users.noreply.github.com>
2020-06-01 15:03:57 -0400

    Update src/install.js
    Co-authored-by: Alex Lewitt <48691328+alewitt2@users.noreply.github.com>

--- src/install.js
+++ src/install.js
@@ -70,7 +70,7 @@ async function main() {
 --rd-cluster-id, --razeedash-cluster-id=''
     : cluster id to be stored into watch-keeper-config ConfigMap and used as the cluster id in RazeeDash instead of namespace.metadata.uid.
 --rd-cluster-metadata64, --razeedash-cluster-metadata64=''
-    : razee cluster metadata JSON base64 encoded string, <key, value> pairs to be stored into watch-keeper-cluster-metadata config-map
+    :  base64 encoded JSON object of cluster metadata entries {key: value, ...}. To be stored into watch-keeper-cluster-metadata ConfigMap and sent to RazeeDash.
 --rr, --remoteresource=''
     : install remoteresource at a specific version (Default 'latest')
 --rrs3, --remoteresources3=''
